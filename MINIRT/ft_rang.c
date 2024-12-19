b7f71ff52777d4fdf3df270040fdd17020e2c059
Youssef Jablaoui <yojablao@e1r11p1.1337.ma>
2024-12-19 20:44:14 +0100

    first

---
+++ MINIRT/ft_rang.c
@@ -0,0 +1,12 @@
+#include "tool.h"
+
+
+
+
+int ft_rang(double min,double num,double max)
+{
+    if (num > min && num < max)
+        return (1);
+    else 
+        return (0);
+}
--- MINIRT/main.c
+++ MINIRT/main.c
@@ -25,18 +25,52 @@ int check_file(char *s,int *fd)
     }
     return (1);
 }
+int check_identifier(char *s)
+{
+    if(ft_strncmp(s,"A",1) == 0)
+        return (1);
+    else if(ft_strncmp(s,"C",1) == 0)
+        return (2);
+    else if(ft_strncmp(s,"L",1) == 0)
+        return (3);
+    else if(ft_strncmp(s,"sp",1) == 0)
+        return (4);
+    else if(ft_strncmp(s,"pl",1) == 0)
+        return (5);
+    else if(ft_strncmp(s,"cy",1) == 0)
+        return (6);
+    else
+        return (0);
+}
 
-void pars_it(char *s,t_scene *scene)
+
+int fill_struct(t_scene *scene,char **buffer,int type)
+{
+    if (type == 1)
+        fill_ambligth(&scene->amligth,);
+    else if (type == 2)
+
+    else if (type == 3)
+
+    else if (type == 4)
+
+    else if (type == 5)
+
+    else if (type == 6)
+
+
+}
+int pars_it(char *s,t_scene *scene)
 {
     char **buffer;
     buffer = ft_split(s,' ');
-    int i = 0;
-    while (buffer[i])
-    {
-        printf("%s ",buffer[i]);
-        i++;
-    }
-    printf("\n");
+    int type;
+    type = check_identifier(buffer[0]);
+    if(!type)
+        return(0);
+    fill_struct(scene,buffer,type)
+    
+    
     
 
 
@@ -54,7 +88,8 @@ t_scene *read_it(int fd)
         buffer = get_next_line(fd);
         if(!buffer)
             break;
-        pars_it(buffer,scene);
+        if(!pars_it(buffer,scene))
+            break;
     }
     printf("done_hire\n");
     return (scene);
@@ -71,7 +106,6 @@ int main(int c , char **v)
     {
         if(check_file(v[1],&fd))
             return(printf("error\n"), 1);
-        printf("%d\n",fd);
         scene = read_it(fd);
         
 
